39ef68d7e0fd6f7139201e0b7be5588c0b533c1f
Kalpesh More <90396306+kalpeshmore1234@users.noreply.github.com>
2025-02-09 18:05:42 +0530

    changes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,29 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https://form-strapi-backend.onrender.com"],
+          "img-src": ["'self'", "data:", "blob:", "https://form-strapi-backend.onrender.com"],
+          "media-src": ["'self'", "data:", "blob:", "https://form-strapi-backend.onrender.com"],
+          "script-src": ["'self'", "'unsafe-inline'", "https://form-strapi-backend.onrender.com"],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // OR replace '*' with ["https://your-nextjs-domain.com"] for security
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
